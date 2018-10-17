2097b2eefff4b04b96ffc67941c3fa53b5cf2bb9
Edwin Tumax <edwintumax@gmail.com>
2018-10-17 08:36:58 -0600

    Mostrando todas las imagenes en la pagina

--- js/app.js
+++ js/app.js
@@ -4,36 +4,63 @@ var lapiz = canvas.getContext('2d');
 var fondo = {
     url: './imagenes/tile.png',
     imagen: Image,
+    cargaOk: false
 };
 
 var vaca = {
     url: './imagenes/vaca.png',
-    imagen: Image
+    imagen: Image,
+    cargaOk: false
 };
 
 var cerdo = {
     url: './imagenes/cerdo.png',
-    imagen: Image
+    imagen: Image,
+    cargaOk: false
 };
+
+var llave = {
+    url: './imagenes/llave.png',
+    imagen: Image,
+    cargaOk: false
+}
 
 fondo.imagen = new Image();
 vaca.imagen = new Image();
 cerdo.imagen = new Image();
+llave.imagen = new Image();
 fondo.imagen.src = fondo.url;
 vaca.imagen.src = vaca.url;
 cerdo.imagen.src = cerdo.url;
+llave.imagen.src = llave.url;
 fondo.imagen.addEventListener("load",function(){
-    lapiz.drawImage(fondo.imagen,0,0);
+    fondo.cargaOk = true;
+    dibujar();
 });
 vaca.imagen.addEventListener("load",function(){
-    lapiz.drawImage(vaca.imagen,150,100);
+    vaca.cargaOk = true;
+    dibujar();
 });
-z
 cerdo.imagen.addEventListener("load",function(){
-    lapiz.drawImage(cerdo.imagen,250,200);
+    cerdo.cargaOk = true;
+    dibujar();
+});
+llave.imagen.addEventListener("load",function(){
+    llave.cargaOk = true;
+    dibujar();
 });
 function dibujar(){
-    lapiz.drawImage(fondo.imagen,0,0);
-    lapiz.drawImage(vaca.imagen,150,100);    
+    if(fondo.cargaOk == true){
+        lapiz.drawImage(fondo.imagen,0,0);
+    }
+    if(vaca.cargaOk == true){
+        lapiz.drawImage(vaca.imagen,150,100);
+    }
+    if(cerdo.cargaOk == true){
+        lapiz.drawImage(cerdo.imagen,250,200);
+    }
+    if(llave.cargaOk == true){
+        lapiz.drawImage(llave.imagen,50,50);
+    }
 }
 
